d1a28af2972c2d17815aacaaaf595a0794b2a629
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-06-02 00:54:58 -0300

    AuthValidator user from JWT.

--- src/middlewares/auth.rs
+++ src/middlewares/auth.rs
@@ -1,4 +1,4 @@
-use crate::models::user::Claims;
+use crate::models::user::{User, Claims};
 use actix_web::error::ErrorUnauthorized;
 use actix_web::{dev, Error, FromRequest, HttpRequest};
 use futures::future::{err, ok, Ready};
@@ -7,37 +7,44 @@ use std::env;
 
 #[derive(Debug, Deserialize)]
 pub struct AuthValidator {
-  authenticated: bool
+  pub authenticated: bool,
+  pub user: User
 }
 
 impl FromRequest for AuthValidator {
-    type Error = Error;
-    type Future = Ready<Result<Self, Self::Error>>;
-    type Config = ();
+  type Error = Error;
+  type Future = Ready<Result<Self, Self::Error>>;
+  type Config = ();
 
-    fn from_request(_req: &HttpRequest, _payload: &mut dev::Payload) -> Self::Future {
-      let _auth = _req.headers().get("Authorization");
+  fn from_request(_req: &HttpRequest, _payload: &mut dev::Payload) -> Self::Future {
+    let _auth = _req.headers().get("Authorization");
 
-      match _auth {
-        Some(_) => {
-          let _split: Vec<&str> = _auth.unwrap().to_str().unwrap().split("Bearer").collect();
-          let token = _split[1].trim();
-          
-          let _var = env::var("SECRET_KEY").unwrap().to_string();
-          let key = _var.as_bytes();
+    match _auth {
+      Some(_) => {
+        let _split: Vec<&str> = _auth.unwrap().to_str().unwrap().split("Bearer").collect();
+        let token = _split[1].trim();
+        
+        let _var = env::var("SECRET_KEY").unwrap().to_string();
+        let key = _var.as_bytes();
 
-          match decode::<Claims>(
-            token,
-            &DecodingKey::from_secret(key),
-            &Validation::new(Algorithm::HS256),
-          ) {
-            Ok(_token) => ok(AuthValidator {
-              authenticated: true
-            }),
-            Err(_e) => err(ErrorUnauthorized("Invalid token.")),
-          }
+        match decode::<Claims>(
+          token,
+          &DecodingKey::from_secret(key),
+          &Validation::new(Algorithm::HS256),
+        ) {
+          Ok(_token) => {
+            let claim: Claims = _token.claims;
+            let user: User = serde_json::from_str(&claim.sub).unwrap();
+
+            return ok(AuthValidator {
+              authenticated: true,
+              user: user.clone()
+            });
+          },
+          Err(_e) => err(ErrorUnauthorized("Invalid token.")),
         }
-        None => err(ErrorUnauthorized("Authentication required.")),
       }
+      None => err(ErrorUnauthorized("Authentication required.")),
     }
+  }
 }
