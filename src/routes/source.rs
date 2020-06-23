048784d0b237e9444dfe7790d918c64fe2afc8a7
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-06-23 01:28:23 -0300

    GET source.
    Adjust in API returns.

--- src/routes/source.rs
+++ src/routes/source.rs
@@ -1,4 +1,6 @@
 use cdrs::query::*;
+use cdrs::query_values;
+use cdrs::frame::traits::TryFromRow;
 
 use crate::common::models::app::{Environment, SOURCE_DATA_TOPIC, SOURCE_DATA_ACK_TOPIC};
 use crate::common::models::response::{Response, DataResponse, DataResponseWithTopics};
@@ -9,9 +11,12 @@ use crate::middlewares::auth::AuthValidator;
 use std::sync::Arc;
 
 use log::{info};
-use actix_web::{put, web, HttpResponse};
+use actix_web::{get, put, web, HttpResponse};
 
 use std::collections::HashMap;
+
+use uuid::Uuid;
+use blob_uuid::to_uuid;
 
 /// Create a data source in an element of the twin instance.
 #[put("")]
@@ -22,7 +27,7 @@ async fn put_source(
   register: web::Json<SourceRegister>
 ) -> HttpResponse {
   let _source = Source {
-    id: uuid::Uuid::new_v4(),
+    id: Uuid::new_v4(),
     name: register.name.to_string(),
     element: register.element,
     // created_at: chrono::offset::Utc::now()
@@ -37,7 +42,7 @@ async fn put_source(
       topics.insert(SOURCE_DATA_TOPIC.to_string(), source.clone().data_topic());
       topics.insert(SOURCE_DATA_ACK_TOPIC.to_string(), source.clone().data_ack_topic());
 
-      HttpResponse::Ok().json(DataResponseWithTopics {
+      HttpResponse::Created().json(DataResponseWithTopics {
         topics,
         message: answer,
         status: true,
@@ -45,7 +50,7 @@ async fn put_source(
       })
     },
     Err(_) => {
-      HttpResponse::Ok().json(Response {
+      HttpResponse::InternalServerError().json(Response {
         message: format!("Error in creating source."),
         status: false
       })
@@ -65,6 +70,63 @@ fn insert_source(session: web::Data<Arc<CurrentSession>>, source: &Source) -> Re
   }
 }
 
+fn get_source_by_id(session: web::Data<Arc<CurrentSession>>, source: String) -> Result<Source, (String, usize)> {
+  let id: Uuid;
+  
+  match Uuid::parse_str(&source) {
+    Ok(_id) => { id = _id },
+    Err(_error) => {
+      match to_uuid(&source) {
+        Ok(_id) => { id = _id },
+        Err(_) => { return Err((format!("Invalid input source."), 400)); }
+      }
+    }
+  }
+
+  let query = format!("SELECT * FROM source WHERE id = {}", id);
+  info!("{}", query);
+  let r = session.query(query);
+
+  let rows = r.expect("Get source by id")
+    .get_body().unwrap()
+    .into_rows().unwrap();
+
+  if rows.is_empty() {
+    return Err(("No source found.".to_string(), 404));
+  }
+  return Ok(Source::try_from_row(rows[0].clone()).unwrap());
+}
+
+#[get("{source_id}")]
+async fn get_source(
+  _auth: AuthValidator,
+  session: web::Data<Arc<CurrentSession>>,
+  source_id: web::Path<String>
+) -> HttpResponse {
+  match get_source_by_id(session, source_id.to_string()) {
+    Ok(source) => HttpResponse::Ok().json(DataResponse {
+      message: format!("Found source {}", source.clone().id),
+      data: source,
+      status: true
+    }),
+    Err((error, status)) => {
+      let mut response;
+
+      match status {
+        400 => response = HttpResponse::BadRequest(),
+        404 => response = HttpResponse::NotFound(),
+        _ => response = HttpResponse::BadRequest()
+      }
+      
+      response.json(Response {
+        message: error,
+        status: false
+      })
+    }
+  }
+}
+
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
+  cfg.service(get_source);
   cfg.service(put_source);
 }
