61e12e84b75961a32fd0c8cdacc14c9f5d29e99a
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-07-05 18:55:56 -0300

    Delete source tests.

--- tests/source.rs
+++ tests/source.rs
@@ -18,7 +18,7 @@ use common::models::twin::{Element, ElementRegister, Source, SourceRegister};
 use common::models::user::{Register, UserLogin};
 use common::models::app::{SOURCE_DATA_ACK_TOPIC,SOURCE_DATA_TOPIC};
 use common::db::get_db_session;
-use common::requests::{get, post, put};
+use common::requests::{get, post, put, delete};
 
 #[test]
 /// Register new source of element.
@@ -172,4 +172,140 @@ fn create_source() {
   assert_eq!(resp_source_2_body.data.name, resp_3_body.data.name);
   assert_eq!(resp_source_2_body.data.element, resp_3_body.data.element);
   assert_eq!(resp_source_2_body.data, resp_3_body.data);
+}
+
+
+#[test]
+/// Delete a created source.
+fn delete_source() {
+  let session = get_db_session();
+
+  let user_1 = Register {
+    email: "example11@example.co.uk".to_string(),
+    name: "User name".to_string(),
+    password: "qWeRtY123$".to_string()
+  };
+  session.query(format!("DELETE FROM user WHERE email='{}'", user_1.email).to_string()).unwrap();
+
+  let resp_1 = post("user/register")
+    .json(&user_1).send().unwrap();
+
+  assert_eq!(resp_1.status(), StatusCode::OK);
+  let resp_1_body: Response = resp_1.json().unwrap();
+  assert_eq!(resp_1_body.status, true);
+
+  let login = UserLogin {
+    email: user_1.email.to_string(),
+    password: user_1.password.to_string(),
+    remember_me: false
+  };
+
+  let resp_login = post("user/login")
+    .json(&login).send().unwrap();
+
+  assert_eq!(resp_login.status(), StatusCode::OK);
+
+  let resp_login_json: LoginResponse = resp_login.json().unwrap();
+  assert!(resp_login_json.status);
+  let token = resp_login_json.token;
+
+  // Create element
+  let element_register_1 = ElementRegister {
+    name: "Element who will have no source".to_string(),
+    parent: None
+  };
+
+  let resp_2 = put("element").bearer_auth(&token)
+    .json(&element_register_1).send().unwrap();
+
+  assert_eq!(resp_2.status(), StatusCode::CREATED);
+
+  let resp_2_body: DataResponse<Element> = resp_2.json().unwrap();
+  assert_eq!(resp_2_body.status, true);
+
+  let element = resp_2_body.data;
+  assert_eq!(element.name, element_register_1.name);
+  assert_eq!(element.parent, element_register_1.parent);
+
+  // Create data source in element
+  let source_register_1 = SourceRegister {
+    name: "Source to be deleted".to_string(),
+    element: element.id
+  };
+
+  let resp_3 = put("source").bearer_auth(&token)
+    .json(&source_register_1).send().unwrap();
+
+  assert_eq!(resp_3.status(), StatusCode::CREATED);
+
+  let resp_3_body: DataResponseWithTopics<Source> = resp_3.json().unwrap();
+  assert_eq!(resp_3_body.status, true);
+
+  let source = resp_3_body.data;
+  assert_eq!(source.name, source_register_1.name);
+  assert_eq!(source.element, source_register_1.element);
+  assert_eq!(source.element, element.id);
+
+  // Get source from created source object id
+  let resp_source = get(format!("source/{}", source.id).as_str()).bearer_auth(&token).send().unwrap();
+  assert_eq!(resp_source.status(), StatusCode::OK);
+
+  let resp_source_body: DataResponse<Source> = resp_source.json().unwrap();
+  assert_eq!(resp_source_body.status, true);
+  assert_eq!(resp_source_body.data.id, source.id);
+  assert_eq!(resp_source_body.data.name, source.name);
+  assert_eq!(resp_source_body.data.element, source.element);
+  assert_eq!(resp_source_body.data, source);
+
+  // Insert source data into source history.
+  session.query(format!(
+    "INSERT INTO source_data (source, stamp, value, created_at) VALUES ({}, toTimestamp(now()), 'value1', toTimestamp(now()))",
+    source.id
+  )).unwrap();
+
+  session.query(format!(
+    "INSERT INTO source_data (source, stamp, value, created_at) VALUES ({}, toTimestamp(now()), 'value2', toTimestamp(now()))",
+    source.id
+  )).unwrap();
+
+  let source_data_rows = session.query(format!("SELECT * FROM source_data WHERE source = {}", source.id)).unwrap()
+    .get_body().unwrap()
+    .into_rows().unwrap();
+
+  assert_eq!(source_data_rows.len(), 2);
+
+  // Delete created source
+  let resp_delete = delete(format!("source/{}", source.id).as_str()).bearer_auth(&token).send().unwrap();
+  assert_eq!(resp_delete.status(), StatusCode::OK);
+
+  let resp_delete_body: Response = resp_delete.json().unwrap();
+  assert_eq!(resp_delete_body.status, true);
+
+  // Get deleted source does not fail, but returns OK.
+  let resp_source_deleted = get(format!("source/{}", source.id).as_str()).bearer_auth(&token).send().unwrap();
+  assert_eq!(resp_source_deleted.status(), StatusCode::NOT_FOUND);
+
+  let resp_source_deleted_body: Response = resp_source_deleted.json().unwrap();
+  assert_eq!(resp_source_deleted_body.status, false);
+
+  // Delete on deleted source should not fail
+  let resp_source_deleted2 = delete(format!("source/{}", source.id).as_str()).bearer_auth(&token).send().unwrap();
+  assert_eq!(resp_source_deleted2.status(), StatusCode::OK);
+
+  let resp_source_deleted2_body: Response = resp_source_deleted2.json().unwrap();
+  assert_eq!(resp_source_deleted2_body.status, true);
+
+  // Source data from deleted source should be deleted.
+  let source_data_rows2 = session.query(format!("SELECT * FROM source_data WHERE source = {}", source.id)).unwrap()
+    .get_body().unwrap()
+    .into_rows().unwrap();
+
+  assert_eq!(source_data_rows2.len(), 0);
+
+  // Delete on invalid input should fail
+  let resp_source_deleted3 = delete("source/123").bearer_auth(&token).send().unwrap();
+  assert_eq!(resp_source_deleted3.status(), StatusCode::BAD_REQUEST);
+
+  let resp_source_deleted3_body: Response = resp_source_deleted3.json().unwrap();
+  assert_eq!(resp_source_deleted3_body.status, false);
 }
